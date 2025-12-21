6ca60ac10c0a3ddc6f1239def4e1628b5e198398
biu991226-gif <biu991226@hotmail.com>
2025-12-21 22:05:56 +0900

    调整

--- ckwuser/src/main/java/ckwuser/Login.java
+++ ckwuser/src/main/java/ckwuser/Login.java
@@ -72,14 +72,6 @@ public class Login extends HttpServlet {
 			    String sql = "insert into users (firstName,lastName,email,password) values ('" 
 			    + firstName + "', '" + lastName + "', '" + email + "', '" + password + "')";
 			    int num = stmt.executeUpdate(sql);
-//			$link = mysqli_connect("mysql3101.db.sakura.ne.jp", "mzx991226_zhixin", "mzx991226", "mzx991226_zhixin");
-	//
-//			$sql ="INSERT INTO `users`(`firstName`,`lastName`,`email`, `password`) VALUES ('$firstname','$lastname','$email','$password ')";
-//			$res = mysqli_query($link, $sql);
-	//
-	//
-//			echo "注册成功";
-//			echo "<a href='login.html'>登录</a>";
 			    
 			    response.setContentType("text/html; charset=UTF-8");
 			    
@@ -126,23 +118,6 @@ public class Login extends HttpServlet {
 			    	session.setAttribute("email", email);
 			    }
 
-
-	//
-//			$link = mysqli_connect("mysql3101.db.sakura.ne.jp", "mzx991226_zhixin", "mzx991226", "mzx991226_zhixin");
-	//
-//			$sql ="SELECT email, password FROM `users` 
-//			WHERE 
-//			email='$email' and password='$password'";
-//			$res = mysqli_query($link, $sql);
-	//
-//			if($res->num_rows==0){
-	//
-//			echo "这个用户不存在！".$sql;
-//			}else{ 
-//			$_SESSION["email"]= $email;
-//			echo "登录成功";
-//			echo "<a href='my.php'>自己的信息</a>";
-//			}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
